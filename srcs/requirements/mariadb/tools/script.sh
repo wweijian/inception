b024e26447202af26f0d42345af50c954b63f9c3
wweijian <hoe.weijian@gmail.com>
2025-09-28 11:45:59 +0800

    mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-mysql_upgrade
-mysqld
+INIT='/tmp/init.sql'
+
+echo "USE mysql;" > ${INIT}
+echo "FLUSH PRIVILEGES;" >> ${INIT}
+echo "DELETE FROM mysql.user WHERE User='';" >> ${INIT}
+echo "DROP DATABASE IF EXISTS test;" >> ${INIT}
+echo "DELETE FROM mysql.db WHERE Db='test';" >> ${INIT}
+echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${INIT}
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> ${INIT}
+echo "CREATE DATABASE ${WP_DB_NAME};" >> ${INIT}
+echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${INIT}
+echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${INIT}
+echo "FLUSH PRIVILEGES;" >> ${INIT}
+
+cp tmp/init.sql /etc/mysql/init.sql
+exec /usr/bin/mysqld --user=mysql --console
