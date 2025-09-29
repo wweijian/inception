83357b0ffcc67d1b608233aa241241e0bec77757
wweijian <hoe.weijian@gmail.com>
2025-09-29 08:53:22 +0800

    .

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,50 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "[DB config] Configuring MariaDB..."
+set -e
 
-if [ ! -d "/run/mysqld" ]; then
-	echo "[DB config] Granting MariaDB daemon run permissions..."
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+if [ ! -d "var/lib/mysql/mysql" ]; then
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
-if [ -d "/var/lib/mysql/mysql" ]
-then
-	echo "[DB config] MariaDB already configured."
-else
-	echo "[DB config] Installing MySQL Data Directory..."
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-	echo "[DB config] MySQL Data Directory done."
-
-	echo "[DB config] Configuring MySQL..."
-	TMP=/tmp/.tmpfile
-
-	echo "USE mysql;" > ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
-	echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
-	echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> ${TMP}
-	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
-	echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-
-	# Alpine does not come with service or rc-service,
-	# so we cannot use: service mysql start
-	# We might be able to install with: apk add openrc
-	# But we can also manually start and configure the mysql daemon:
-	/usr/sbin/mysqld --user=mysql --bootstrap < ${TMP}
-	touch /etc/mysql/init.sql
-	rm -f ${TMP}
-	echo "[DB config] MySQL configuration done."
-fi
-
-echo "[DB config] Allowing remote connections to MariaDB"
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-echo "[DB config] Starting MariaDB daemon on port 3306."
-exec /usr/sbin/mysqld --user=mysql --console
+exec mysqld_safe --datadir='var/lib/mysql'
